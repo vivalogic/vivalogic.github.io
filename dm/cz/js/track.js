b4c632ce9258ac90d8908480c0b6f80a74919f7f
Jaromir Kalab <kalabj@mini.local>
2018-10-23 21:34:52 +0200

    GA pro zbytek webu
    - pridal jsem GA pro zbytek dm webu
    - presunul jsem track.js do adresare pro js

---
+++ dm/cz/js/track.js
@@ -0,0 +1,43 @@
+$("a[data-track='btn_quiz']").on('click', function () {
+    dataLayer.push({
+        'event': 'gaVirtualPageview',
+        'gaVirtualPageviewUrl': '/dm/cz/dekorkozmetika/oktober-true-match/test/testintroduction',
+        'gaVirtualPageviewTitle': '‘L’Oreal | Test step 01 – Test introduction'
+    });
+    gtag('config', 'UA-74716905-5', {'page_path': '/dm/cz/dekorkozmetika/oktober-true-match/test/testintroduction'});
+});
+
+$("#quiz_1 a[data-track='quiz_next']").on('click', function () {
+    dataLayer.push({
+        'event': 'gaVirtualPageview',
+        'gaVirtualPageviewUrl': '/dm/cz/dekorkozmetika/oktober-true-match/test/haircolor',
+        'gaVirtualPageviewTitle': '‘L’Oreal | Test step 02 - Hair color'
+    });
+    gtag('config', 'UA-74716905-5', {'page_path': '/dm/cz/dekorkozmetika/oktober-true-match/test/haircolor'});    
+});
+
+$("#quiz_2 a[data-track='quiz_next']").on('click', function () {
+    dataLayer.push({
+        'event': 'gaVirtualPageview',
+        'gaVirtualPageviewUrl': '/dm/cz/dekorkozmetika/oktober-true-match/test/skincolor',
+        'gaVirtualPageviewTitle': '‘L’Oreal | Test step 03 - Skin color'
+    });
+    gtag('config', 'UA-74716905-5', {'page_path': '/dm/cz/dekorkozmetika/oktober-true-match/test/skincolor'});
+});
+
+$("#quiz_3 a[data-track='quiz_next']").on('click', function () {
+    dataLayer.push({
+        'event': 'gaVirtualPageview',
+        'gaVirtualPageviewUrl': '/dm/cz/dekorkozmetika/oktober-true-match/test/modeltype',
+        'gaVirtualPageviewTitle': '‘L’Oreal | Test step 04 - Model type'
+    });
+    gtag('config', 'UA-74716905-5', {'page_path': '/dm/cz/dekorkozmetika/oktober-true-match/test/modeltype'});
+    setTimeout(function () {
+        dataLayer.push({
+            'event': 'gaVirtualPageview',
+            'gaVirtualPageviewUrl': '/dm/cz/dekorkozmetika/oktober-true-match/test/testcompleted',
+            'gaVirtualPageviewTitle': '’L’Oreal | Test step 05 - Test completed'
+        });
+        gtag('config', 'UA-74716905-5', {'page_path': '/dm/cz/dekorkozmetika/oktober-true-match/test/testcompleted'});
+    },333);
+});
